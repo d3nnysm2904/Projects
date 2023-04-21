ae8ae9ff907d832aaec775d9f59192f502829d18
d3nnysm2904 <dennymosquera@outlook.com>
2023-04-20 20:33:04 -0400

    prob-divide-conquer

---
+++ prob-divide-conquer/find-rotation-count.js
@@ -0,0 +1,26 @@
+function findRotationCount ( arr )
+{
+    idxLeft = 0;
+    idxRigh = arr.length - 1;
+    let middle = Math.floor( ( idxLeft + idxRigh ) / 2 );
+
+    while ( idxRigh != -1 )
+    {
+        middle = Math.floor( ( idxLeft + idxRigh ) / 2 );
+        if ( arr[ middle - 1 ] > arr[ middle ] )
+        {
+            return middle;
+        } else
+        {
+            idxLeft = middle + 1;
+        }
+        if ( idxLeft === arr.length ) return 0;
+        idxLeft = middle + 1;
+    }
+}
+
+console.log( "Case1: ", findRotationCount( [ 15, 18, 2, 3, 6, 12 ] ) ); // 2
+console.log( "Case2: ", findRotationCount( [ 7, 9, 11, 12, 5 ] ) ); // 4
+console.log( "Case3: ", findRotationCount( [ 7, 9, 11, 12, 15 ] ) ); // 0
+
+module.exports = findRotationCount;
