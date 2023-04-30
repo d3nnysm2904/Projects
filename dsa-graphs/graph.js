8a9daa9be8f475f915cd1b5e4bbd5b15a26b5b88
d3nnysm2904 <dennymosquera@outlook.com>
2023-04-30 10:27:33 -0400

    graphs

--- dsa-graphs/graph.js
+++ dsa-graphs/graph.js
@@ -1,35 +1,123 @@
-class Node {
-  constructor(value, adjacent = new Set()) {
-    this.value = value;
+class PersonNode
+{
+  constructor ( name, adjacent = new Set() )
+  {
+    this.name = name;
     this.adjacent = adjacent;
   }
 }
 
-class Graph {
+class FriendGraph
+{
   constructor() {
     this.nodes = new Set();
   }
+  addPerson ( node )
+  {
+    this.nodes.add( node );
+  }
+  addPeople ( peopleList )
+  {
+    for ( const node of peopleList )
+    {
+      this.addPerson( node );
+    }
+  }
+  setFriends ( p1, p2 )
+  {
+    p1.adjacent.add( p2 );
+    p2.adjacent.add( p1 );
+  }
 
-  // this function accepts a Node instance and adds it to the nodes property on the graph
-  addVertex(vertex) {}
+  // BFS using a queue, shift()
+  areConnectedBFS ( p1, p2 )
+  {
+    let toVisitQueue = [ p1 ];
+    let seen = new Set( toVisitQueue );
+    while ( toVisitQueue.length )
+    {
+      let currPerson = toVisitQueue.shift();
+      console.log( "VISITING: ", currPerson.name );
+      if ( currPerson === p2 ) return true;
+      for ( const neighbor of currPerson.adjacent )
+      {
+        if ( !seen.has( neighbor ) )
+        {
+          toVisitQueue.push( neighbor );
+          seen.add( neighbor );
+        }
+      }
+    }
+    return false;
+  }
+  // DFS using a stack, pop()
+  // tranverse
+  areConnectedDFS ( p1, p2 )
+  {
+    let toVisitStack = [ p1 ];
+    let seen = new Set( toVisitStack );
+    while ( toVisitStack.length )
+    {
+      let currPerson = toVisitStack.pop();
+      console.log( "VISITING: ", currPerson.name );
+      if ( currPerson === p2 ) return true;
+      for ( const neighbor of currPerson.adjacent )
+      {
+        if ( !seen.has( neighbor ) )
+        {
+          toVisitStack.push( neighbor );
+          seen.add( neighbor );
+        }
+      }
+    }
+    return false;
+  }
 
-  // this function accepts an array of Node instances and adds them to the nodes property on the graph
-  addVertices(vertexArray) {}
-
-  // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
-
-  // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
-
-  // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
-
-  // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
-
-  // this function returns an array of Node values using BFS
-  breadthFirstSearch(start) {}
+  areConnectedRecursive ( p1, p2, seen = new Set( [ p1 ] ) )
+  {
+    if ( p1 === p2 ) return true;
+    for ( const neighbor of p1.adjacent )
+    {
+      if ( !seen.has( neighbor ) )
+      {
+        seen.add( neighbor );
+        console.log( "VISITING: ", neighbor.name );
+        if ( this.areConnectedRecursive( neighbor, p2, seen ) )
+        {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
 }
 
-module.exports = {Graph, Node}
+const homer = new PersonNodee( "Homer Simpson" );
+const marge = new PersonNodee( "Marge Simpson" );
+const magie = new PersonNodee( "Magie Simpson" );
+const lisa = new PersonNodee( "Lisa Simpson" );
+const grampa = new PersonNodee( "Grampa Simpson" );
+
+const moe = new PersonNodee( "Moe" );
+const barney = new PersonNodee( "Barney" );
+const lenny = new PersonNodee( "Lenny" );
+
+const friends = new FriendGraph();
+friends.addPeople( [ homer, marge, magie, lisa, grampa ] );
+friends.setFriends( homer, marge );
+friends.setFriends( homer, lisa );
+friends.setFriends( homer, magie );
+friends.setFriends( marge, magie );
+friends.setFriends( magie, lisa );
+friends.setFriends( lisa, grampa );
+friends.addPeople( [ moe, barney, lenny ] );
+friends.setFriends( moe, barney );
+friends.setFriends( barney, lenny );
+
+// console.log(friends);
+// for (const f of friends.nodes) {
+//   console.log(f);
+// }
+console.log( friends.areConnectedBFS( homer, marge ) );
+console.log( friends.areConnectedDFS( homer, marge ) );
+console.log( friends.areConnectedRecursive( homer, barney ) );
